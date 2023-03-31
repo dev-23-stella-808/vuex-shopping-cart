f1928137d90bcb3665afb4e61d9a2994378ffae5
dev-23-stella-808 <dev_23_user808@proton.me>
2023-03-31 01:21:39 -0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <NavBar/>
+    <div class ="container mt-6">
+      <div class="columns">
+        <div class="column is-12 column--align-center">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/core/NavBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+html, body {
+  height:100%;
+  background:#f2f6fa;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import {createRouter,createWebHistory} from 'vue-router'
+
+import CartList from '../components/cart/Cart_List.vue';
+import ProductList from '../components/product/Product_List.vue';
+
+const routes = [
+    {
+        path:'/inventory',
+        component:ProductList,
+    },
+    {
+        path:'/cart',
+        component:CartList,
+    },
+    {
+        path:'/',
+        redirect:'/inventory'
+    }
+]
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+})
+
+export default router
